dae57db14b5d7fff996cb6e312856c9944983a94
hintsete <hintseteabera@gmail.com>
2025-01-06 15:25:20 +0300

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
